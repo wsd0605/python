9bda60990a879ab569270dfad730a63976940d94
wsd0605 <36854788+wsd0605@users.noreply.github.com>
2018-03-03 17:35:38 +0800

    Add files via upload
    this is from SpiderBook CH18

---
+++ t.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 21:22:38 2018
+
+@author: Administrator
+"""
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    {
+         print("‐‐ This parrot wouldn't", action, end=' ')
+         print("if you put", voltage, "volts through it.")
+         print("‐‐ Lovely plumage, the", type)   
+         print("‐‐ It's", state, "!")   
+   }
+    
+    parrot(11)
+    
